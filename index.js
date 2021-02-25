31bed0869f2d3903d11645fc0523bc632df3c7a2
joaoderocha <jharocha@gmail.com>
2021-02-24 22:53:45 -0300

    fix: corrige regex

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-'use strict';
+"use strict";
 
-const {publish} = require(
-    '@joaoderocha/redis-pub-sub-nodejs/headless/redis/services/pub-sub',
-);
 const {
-  messageBuilder, getNextStep, roundRobinSubscribe, TERMCLEAN,
-} = require('@joaoderocha/redis-pub-sub-nodejs/headless/utils');
+  publish,
+} = require("@joaoderocha/redis-pub-sub-nodejs/headless/redis/services/pub-sub");
+const {
+  messageBuilder,
+  getNextStep,
+  roundRobinSubscribe,
+  TERMCLEAN,
+} = require("@joaoderocha/redis-pub-sub-nodejs/headless/utils");
 
-const cleanTerms = (linha) => linha.normalize('NFD');
+const cleanTerms = (linha) => linha.normalize("NFD");
 
-const cleanPunctuation = (linha) => linha
-    .replace(/[.*.,+?^${}()~!@#$:|[\]\\]/gi, '');
+const cleanPunctuation = (linha) => {
+  const regex = RegExp(/[^\w\s]/gi);
+
+  return linha.replace(regex, " ");
+};
 
 const termCleanStep = (channel, message) => {
   console.log(message);
 
-  const {linha, queueIndex} = message;
+  const { linha, queueIndex } = message;
 
   const linhaSemAcentos = cleanTerms(linha);
   const linhaLimpa = cleanPunctuation(linhaSemAcentos);
